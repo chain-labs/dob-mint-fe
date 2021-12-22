a6c8506119af2cb29e88b31a55477bc6ebfe51b3
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-12-22 14:36:46 +0530

    Update Loading.jsx

--- src/containers/Loading.jsx
+++ src/containers/Loading.jsx
@@ -4,16 +4,17 @@ import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 const Loading = ({ message }) => (
 	<header className={"App-header"}>
 		<FontAwesomeIcon className={"App-logo logo-spin"} icon={faCircleNotch} />
-		<h3 className={"text-center mt-5"}>
+		<h3 className={"mt-5"}>
 			{message}
 			<div>
+                    <label>Need help?</label>
 				<div>
 					<a href="https://daughtersofblockchain.com/metamask-wallet/">
-						https://daughtersofblockchain.com/metamask-wallet/
+						1. Steps to connect wallet
 					</a>
 				</div>
 				<a href="https://daughtersofblockchain.com/get-matic/">
-					https://daughtersofblockchain.com/get-matic/
+					2. Steps to get Matic
 				</a>
 			</div>
 		</h3> 
